2a32745953424a2af26f3d917cb979bf65a8fb60
Mawuena <iammawuena1@gmail.com>
2022-10-02 17:31:37 +0000

    Functionality added

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: avoid_unnecessary_containers
-
+// ignore_for_file: avoid_unnecessary_containers, unused_local_variable
 import 'package:calculator/components/buttons.dart';
+import 'package:calculator/components/const.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 class Calculator extends StatefulWidget {
   const Calculator({super.key});
@@ -11,6 +12,11 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+  //S TO HOLD Q&A
+  var userQuestion = '';
+  var userAnswer = '';
+
+  //KEYPAD
   final List buttons = [
     'CLR',
     'DEL',
@@ -37,48 +43,108 @@ class _CalculatorState extends State<Calculator> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow[300],
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: Container(),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              child: GridView.builder(
-                itemCount: buttons.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(kDefaultPadding),
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        userQuestion,
+                        style: const TextStyle(fontSize: 20.0),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(kDefaultPadding),
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        userAnswer,
+                        style: const TextStyle(fontSize: 20.0),
+                      ),
+                    ),
+                  ],
                 ),
-                itemBuilder: (BuildContext context, int index) {
-                  if (index == 0) {
-                    return MyButtons(
-                      color: Colors.red[700],
-                      textColor: Colors.black,
-                      buttonText: buttons[index],
-                    );
-                  } else if (index == 1) {
-                    return MyButtons(
-                      color: Colors.blue[700],
-                      textColor: Colors.black,
-                      buttonText: buttons[index],
-                    );
-                  } else {
-                    return MyButtons(
-                      color: isOperators(buttons[index])
-                          ? Colors.black
-                          : Colors.yellow[700],
-                      textColor: isOperators(buttons[index])
-                          ? Colors.white
-                          : Colors.black,
-                      buttonText: buttons[index],
-                    );
-                  }
-                },
               ),
             ),
-          ),
-        ],
+            Expanded(
+              flex: 2,
+              child: Container(
+                child: GridView.builder(
+                  itemCount: buttons.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    // CLEAR BUTTON
+                    if (index == 0) {
+                      return MyButtons(
+                        color: Colors.red[700],
+                        textColor: Colors.black,
+                        buttonText: buttons[index],
+                        buttonPressed: () {
+                          setState(() {
+                            userQuestion = '';
+                          });
+                        },
+                      );
+                    } else if (index == 1) {
+                      // DELETE BUTTON
+                      return MyButtons(
+                        color: Colors.blue[700],
+                        textColor: Colors.black,
+                        buttonText: buttons[index],
+                        buttonPressed: () {
+                          setState(() {
+                            userQuestion = userQuestion.substring(
+                                0, userQuestion.length - 1);
+                          });
+                        },
+                      );
+                    }
+                    //EQUAL BUTTON
+                    else if (index == buttons.length - 1) {
+                      return MyButtons(
+                        color: isOperators(buttons[index])
+                            ? Colors.black
+                            : Colors.yellow[700],
+                        textColor: isOperators(buttons[index])
+                            ? Colors.white
+                            : Colors.black,
+                        buttonText: buttons[index],
+                        buttonPressed: () {
+                          setState(() {
+                            evaluation();
+                          });
+                        },
+                      );
+                    } else {
+                      // OTHER BUTTONS
+                      return MyButtons(
+                        color: isOperators(buttons[index])
+                            ? Colors.black
+                            : Colors.yellow[700],
+                        textColor: isOperators(buttons[index])
+                            ? Colors.white
+                            : Colors.black,
+                        buttonText: buttons[index],
+                        buttonPressed: () {
+                          setState(() {
+                            userQuestion += buttons[index];
+                          });
+                        },
+                      );
+                    }
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -89,5 +155,22 @@ class _CalculatorState extends State<Calculator> {
       return true;
     }
     return false;
+  }
+
+  // EQUAL ASSIGNMENT
+  void evaluation() {
+    String finalQuestion = userQuestion;
+
+    //CONVERTING X TO * FOR MULTIPLICATION PURPOSE
+    finalQuestion = finalQuestion.replaceAll('x', '*');
+
+    //PACKAGES FROM PUB DEV
+    Parser p = Parser();
+    Expression exp = p.parse(finalQuestion);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+    //CONVERTING FINAL RESULT TO STRING
+    userAnswer = eval.toString();
   }
 }
--- lib/components/buttons.dart
+++ lib/components/buttons.dart
@@ -1,32 +1,38 @@
 // ignore_for_file: prefer_typing_uninitialized_variables
 
+import 'package:calculator/components/const.dart';
 import 'package:flutter/material.dart';
 
 class MyButtons extends StatelessWidget {
   final color;
   final textColor;
   final String buttonText;
+  final buttonPressed;
   const MyButtons({
     super.key,
     required this.color,
     required this.textColor,
     required this.buttonText,
+    required this.buttonPressed,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-        child: Container(
-          color: color,
-          child: Center(
-            child: Text(
-              buttonText,
-              style: TextStyle(
-                color: textColor,
-                fontSize: 30,
+    return GestureDetector(
+      onTap: buttonPressed,
+      child: Padding(
+        padding: const EdgeInsets.all(kDefaultPadding / 2),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+            color: color,
+            child: Center(
+              child: Text(
+                buttonText,
+                style: TextStyle(
+                  color: textColor,
+                  fontSize: 30,
+                ),
               ),
             ),
           ),
